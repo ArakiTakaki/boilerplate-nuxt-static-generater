73158a660b822fbe8743803a646302a77257bf6e
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-11-24 14:25:38 +0900

    add to [nuxt-property-decorator, teamlab-eslint-config]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
 import { Configuration } from '@nuxt/types'
 
 const config: Configuration = {
-  // Config設定の記述
-  // ...
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxt/typescript-build' // <- 追加
+    '@nuxt/typescript-build'
   ],
-  // ...
 }
 
 export default config;
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template lang="html">
   <div>
     <h1>hello nuxt</h1>
+    <input id="" type="text" v-model="sample">
+    <div>{$data.sample}</div>
   </div>
 </template>
 
 <script lang="ts">
-module.exports = {}
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Index extends Vue {
+  public sample!: string;
+}
+
 </script>
 
