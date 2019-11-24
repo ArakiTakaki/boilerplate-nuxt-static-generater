0493d680a9f3f6403e05888d887a0f8e3065e02e
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-11-24 15:15:50 +0900

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,28 +2,23 @@ import { Configuration } from '@nuxt/types';
 
 const config: Configuration = {
   srcDir: 'src/',
-  env: {
-    baseUrl: 'http://localhost:3000',
-  },
-  buildModules: [
-    '@nuxt/typescript-build',
-  ],
-  css: [
-    'reset.css',
-  ],
+  env: { baseUrl: 'http://localhost:3000' },
+  buildModules: ['@nuxt/typescript-build'],
+  css: ['reset.css'],
   build: {
     postcss: {
-      plugins: {
-        'postcss-flexbox': {},
-      },
-      preset: {
-        autoprefixer: {
-          grid: true
-        }
-      }
-    }
-  }
-
+      plugins: { 'postcss-flexbox': {} },
+      preset: { autoprefixer: { grid: true } }
+    },
+  },
+  // manifest.json generator
+  manifest: {
+    lang: 'ja',
+    name: 'LP Title',
+    shortName: 'LPT',
+    startUrl: '/',
+    display: 'standalone'
+  },
 };
 
 export default config;
